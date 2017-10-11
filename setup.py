0766c3f28d184677bd5a618fd1328f640aa7b443
Nocorupe <nocorupe@gmail.com>
2017-10-11 17:39:32 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+install_requires = [
+    'gym==0.9.2',
+    'mujoco-py<1.50.2,>=1.50.1',
+]
+
+setup(
+    name='gym-env-mujoco150',
+    version='0.1.0',
+    packages=['gym_env_mujoco150'],
+    url='https://github.com/pfnet/gym-env-mujoco150',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    install_requires=install_requires,
+)
