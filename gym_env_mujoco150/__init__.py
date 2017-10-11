cdf742bceac699b91a1c91f70a02d8dd7cccbbb4
Nocorupe <nocorupe@gmail.com>
2017-10-11 18:20:52 +0900

    add Humanoid

--- gym_env_mujoco150/__init__.py
+++ gym_env_mujoco150/__init__.py
@@ -2,6 +2,7 @@ from gym_env_mujoco150.mujoco_env import MujocoEnv
 from gym_env_mujoco150.inverted_pendulum import InvertedPendulumEnv
 from gym_env_mujoco150.inverted_double_pendulum import InvertedDoublePendulumEnv
 from gym_env_mujoco150.hopper import HopperEnv
+from gym_env_mujoco150.humanoid import HumanoidEnv
 
 from gym.envs.registration import registry, register, make, spec
 
@@ -24,4 +25,10 @@ register(
     entry_point='gym_env_mujoco150:HopperEnv',
     max_episode_steps=1000,
     reward_threshold=3800.0,
+)
+
+register(
+    id='Humanoid-mujoco150-v1',
+    entry_point='gym_env_mujoco150:HumanoidEnv',
+    max_episode_steps=1000,
 )
