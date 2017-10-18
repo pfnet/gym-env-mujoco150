556613b48884a24b02bc23bf5fe3bbb97883298c
Nocorupe <nocorupe@gmail.com>
2017-10-18 19:04:37 +0900

    fix gym version and package data

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@
 from setuptools import setup
 
 install_requires = [
-    'gym==0.9.2',
-    'mujoco-py<1.50.2,>=1.50.1',
+    'gym>=0.9.2',
+    'mujoco-py==1.50.1.22',
 ]
 
 setup(
     name='gym-env-mujoco150',
     version='0.1.0',
     packages=['gym_env_mujoco150'],
+    package_data={'': ['gym_env_mujoco150/assets/*.xml']},
     url='https://github.com/pfnet/gym-env-mujoco150',
     license='',
     author='',
